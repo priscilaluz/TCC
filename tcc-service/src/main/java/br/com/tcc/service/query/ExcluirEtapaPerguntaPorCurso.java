76351413b05ae5a772f270638f322a978fe4a15e
priscilaluz <priscila.farias145@gmail.com>
2017-07-09 22:50:03 -0300

    add etapas - não 100%

---
+++ tcc-service/src/main/java/br/com/tcc/service/query/ExcluirEtapaPerguntaPorCurso.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+package br.com.tcc.service.query;
+
+import br.com.tcc.service.persistence.BusinessQuery;
+
+/**
+ * ExcluirRespostaPorPergunta
+ * @author Priscila
+ */
+public class ExcluirEtapaPerguntaPorEtapa extends BusinessQuery {
+    
+    public ExcluirEtapaPerguntaPorEtapa(Long idCurso) {
+        appendText(" delete from EtapaPergunta ep where ep.etapa.id in ");
+        appendText(" (select e.id from Etapa e where e.curso.id = :idCurso) ");
+        addParameter("idCurso", idCurso);
+    }
+    
+}
