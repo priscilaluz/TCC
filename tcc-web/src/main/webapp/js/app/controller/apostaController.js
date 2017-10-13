8d1fa15dee2186c6298665439a582b3cb3b2924e
Priscila Luz Spassu <priscila.farias145@gmail.com>
2017-10-13 11:45:26 -0300

    jogo aposta

--- tcc-web/src/main/webapp/js/app/controller/apostaController.js
+++ tcc-web/src/main/webapp/js/app/controller/apostaController.js
@@ -5,6 +5,9 @@ function ($scope, $rootScope, $modal, $location, $timeout, Jogo) {
     var pontuacaoInicialFase = pontuacaoInicial;
     var timeoutTempoPorPergunta = null;
     $scope.model = {
+        perdeuJogo: false,
+        pulo: false,
+        dica: false,
         barraPontos: {},
         tempo: tempoPadraoPergunta,
         maxFichas: null,
@@ -12,25 +15,78 @@ function ($scope, $rootScope, $modal, $location, $timeout, Jogo) {
         valorAposta: [],
         imgAumentar: [],
         imgDiminuir: [],
+        resultados: [],
         qntPulo:1,
         qntDica: 1,
         posicao: 0
     };
     
-    $scope.apostar = function () {
-        //$scope.model.valorAposta.push(0);
+    $scope.voltar = function () {
+        $location.path("/jogos");
+    };
+    
+    $scope.apostou = function () {
+        for (var i = 0; i < $scope.model.valorAposta.length; i++) {
+            if ($scope.model.valorAposta[i] > 0){
+                return true;
+            }
+        }
+        return false;
+    };
+    
+    $scope.apostar = function (tempoAcabou) {
+        var respostaCorreta;
+        var apostasFase = ($scope.model.pulo)?[]:angular.copy($scope.model.valorAposta);
         for (var i = 0; i < $scope.model.pergunta.respostas.length; i++) {
+            if ($scope.model.pulo) {
+                apostasFase.push(0);
+            }
             if ($scope.model.pergunta.respostas[i].correta) {
                 pontuacaoInicialFase = $scope.model.pontuacao + ($scope.model.valorAposta[i]*2);
                 $scope.model.pontuacao = pontuacaoInicialFase;
+                respostaCorreta = $scope.model.pergunta.respostas[i];
             }
         }
-        $scope.model.valorAposta = [];
+        if (tempoAcabou && !$scope.apostou()) {
+            $scope.model.pontuacao = $scope.model.pontuacao - 200;
+        }
+        if ($scope.model.pulo) {
+            $scope.model.pontuacao = $scope.model.pontuacao - 100;
+        }
+        if ($scope.model.pontuacao > 0) {
+            $scope.respostaIncorreta = {
+                'background-color': '#ec3e3e'
+            };
+            $scope.respostaCorreta = {
+                'background-color': '#51ec3e'
+            };
+
+            $scope.model.resultados.push({'apostas': apostasFase, 'respostaCorreta': respostaCorreta,
+                'pergunta': $scope.model.pergunta, 'pulo':  $scope.model.pulo, 'dica':  $scope.model.dica, 'tempoAcabou': tempoAcabou});
+            $scope.model.dica = false;
+            $timeout(proximaPergunta, 400);
+        } else {
+            $scope.model.perdeuJogo = true;
+        }
+    };
+    
+    var proximaPergunta = function (){
+        $scope.model.pulo = false;
+        $scope.model.dica = false;
+        $scope.respostaIncorreta = {};
+        $scope.respostaCorreta = {};
+        $scope.model.tempo = tempoPadraoPergunta;
         $scope.model.posicao++;
-        $scope.model.pergunta = $scope.model.perguntas[$scope.model.posicao];
-        inicializarBotao();
         atualizarPorcentagemBarra();
         barraDeProgresso();
+        if (($scope.model.posicao) < $scope.model.perguntas.length) {
+            $scope.model.pergunta = $scope.model.perguntas[$scope.model.posicao];
+            inicializarBotao();
+        } else {
+            $scope.model.pergunta = null;
+            $timeout.cancel(timeoutTempoPorPergunta);
+            $scope.model.resultado = true;
+        }
     };
     
     $scope.diminuirAposta = function (index) {
@@ -55,7 +111,7 @@ function ($scope, $rootScope, $modal, $location, $timeout, Jogo) {
         if ($scope.model.tempo !== 0) {
             timeoutTempoPorPergunta = $timeout(tempoPergunta, 1000);
         } else {
-            addAoRelatorioFinal(null, false, false);
+            $scope.apostar(true);
         }
     };
     
@@ -73,11 +129,7 @@ function ($scope, $rootScope, $modal, $location, $timeout, Jogo) {
         var porcentagem = ($scope.model.posicao+1)/$scope.model.perguntas.length*100;
         $scope.barraProgresso = {width: porcentagem+'%'};
     };
-    
-//    $scope.aumentarPontuacao = function () {
-//        $scope.model.pontuacao = $scope.model.pontuacao*2;
-//        atualizarPorcentagemBarra();
-//    };
+
     var atualizarPorcentagemBarra = function () {
         var porcentagemAtual = 370 - ($scope.model.pontuacao*400/$scope.model.maxFichas);
         $scope.model.barraPontos = {'margin-top':porcentagemAtual};
@@ -92,6 +144,34 @@ function ($scope, $rootScope, $modal, $location, $timeout, Jogo) {
             $scope.model.imgAumentar.push("img/jogos/aposta/botaoAumentar-off.png");
             $scope.model.imgDiminuir.push("img/jogos/aposta/botaoDiminuir-off.png");
         }
+    };
+    
+    $scope.dicaPergunta = function () {
+        $timeout.cancel(timeoutTempoPorPergunta);
+        var dica = "Essa pergunta não tem dica.";
+        if ($scope.model.pergunta.dica){
+            dica = $scope.model.pergunta.dica;
+            $scope.model.dica = true;
+            $scope.model.qntDica--;
+        }
+        var obj = {'dica': dica};
+        $modal.open({
+            templateUrl: 'partials/jogo/dica.html',
+            controller: 'DicaController',
+            resolve: {obj: function () {return obj;}}
+        }).result.then(function (result) {
+            // Modal retorno
+            tempoPergunta();
+        }, function () {
+            // Modal cancelado
+            tempoPergunta();
+        });
+    };
+    
+    $scope.pularPergunta = function () {
+        $scope.model.pulo = true;
+        $scope.model.qntPulo--;
+        $scope.apostar(false);
     };
     
     var init = function () {
