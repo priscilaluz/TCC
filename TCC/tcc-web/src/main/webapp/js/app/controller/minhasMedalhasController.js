f9c6ce61191715e07bcaca3853c6a9c19dbad329
PriscilaLuz <priscila.farias145@gmail.com>
2019-03-03 15:25:03 -0300

    tela de premios

--- TCC/tcc-web/src/main/webapp/js/app/controller/minhasMedalhasController.js
+++ TCC/tcc-web/src/main/webapp/js/app/controller/minhasMedalhasController.js
@@ -5,13 +5,21 @@ tccApp.controller('MinhasMedalhasController', ['$scope', '$rootScope', '$locatio
         }
         $rootScope.telaHomeAluno = false;
         $scope.model = {
-            premios: []
+            premios: [],
+            nenhumPremio: true
         };
         
         var init = function () {
             $rootScope.appLoaded = false;
             Premio.buscarPremioPorIdAluno({'idAluno': $rootScope.usuarioLogado.id}).$promise.then(function (premio) {
                 $scope.model.premio = premio;
+                $scope.model.nenhumPremio = $scope.model.premio.qntForcaVencida===0 && $scope.model.premio.qntCacaPalavrasVencida===0 &&
+                        $scope.model.premio.qntJogoDaVelhaVencida===0 && $scope.model.premio.qntApostaVencida===0 &&
+                        $scope.model.premio.qntQuizVencidos===0 && $scope.model.premio.qntPergFacilCorreta===0 &&
+                        $scope.model.premio.qntPergMediaCorreta===0 && $scope.model.premio.qntPergDificilCorreta===0 &&
+                        $scope.model.premio.qntApostaTudoEmUmaPergVencida===0 && $scope.model.premio.qntEtapaVencida===0 &&
+                        $scope.model.premio.qntEtapaSemPulo===0 && $scope.model.premio.qntEtapaSemDica===0 &&
+                        $scope.model.premio.qntPontosAcumulados===0 && $scope.model.premio.qntCursoConcluido===0;
                 $rootScope.appLoaded = true;
             }, function (error) {
                 $rootScope.appLoaded = true;
